38b476169d5b518f885e4fb41e6280a71b275828
Chris Taylor <chris@ctaylr.net>
2017-09-27 05:07:42 +0000

    actually add files this time ;)

---
+++ gulpfile.js
@@ -0,0 +1,68 @@
+var gulp = require('gulp'),
+	sass = require('gulp-sass'),
+	autoprefixer = require('gulp-autoprefixer'),
+	//jshint = require('gulp-jshint'),
+	uglify = require('gulp-uglify'),
+	imagemin = require('gulp-imagemin'),
+	rename = require('gulp-rename'),
+	concat = require('gulp-concat'),
+	notify = require('gulp-notify'),
+	cache = require('gulp-cache'),
+	livereload = require('gulp-livereload'),
+	connect = require('gulp-connect'),
+	lr = require('tiny-lr'),
+	server = lr();
+
+// Server - listed on localhost:8080
+gulp.task('webserver', function() {
+  connect.server({
+    livereload:true      
+  });
+});
+
+gulp.task('styles', function() {
+  return gulp.src('src/css/**/*')
+	.pipe(sass({ style: 'expanded' }))
+	.pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
+	.pipe(concat('style.css'))
+	.pipe(gulp.dest('./docs/css'))
+});
+
+//move html docs to /docs
+gulp.task('html', function(){
+   return gulp.src('src/*.html')
+         .pipe(gulp.dest('./docs')); 
+});
+
+// Concatenate & Minify JS
+gulp.task('scripts', function() {
+	return gulp.src('src/js/**/*')
+		.pipe(concat('scripts.min.js'))
+		.pipe(uglify())
+		.pipe(gulp.dest('./docs/js'));
+});
+
+// Images
+gulp.task('images', function() {
+  return gulp.src('src/images/**/*')
+    .pipe(cache(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true })))
+    .pipe(gulp.dest('./docs/images'))
+    .pipe(notify({ message: 'Images task complete' }));
+});
+
+// Watch
+gulp.task('watch', function() {
+
+  // Watch .scss files
+  gulp.watch('src/css/**/*.scss', ['styles']);
+
+  // Watch .js files
+  gulp.watch('src/js/**/*.js', ['scripts']);
+
+  // Watch image files
+  gulp.watch('src/images/**/*', ['images']);
+
+  gulp.watch('src/*.html', ['html']);
+});
+
+gulp.task('default', ['webserver','styles', 'scripts', 'watch']);
